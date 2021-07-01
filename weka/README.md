d0215f96cf1c52a829ed42fee9b8ec23e44d7d7c
Graham Smith <gsmitheidw@gmail.com>
2021-07-01 12:10:07 +0100

    first upload

---
+++ weka/README.md
@@ -0,0 +1,28 @@
+# Weka Chocolatey.org package Recipe
+---
+## Weka Machine learning software
+
+Weka is a collection of machine learning algorithms for solving real-world data mining problems. It is written in Java and runs on almost any platform. The algorithms can either be applied directly to a dataset or called from your own Java code.
+
+Features:
+* machine learning
+* data mining
+* preprocessing
+* classification
+* regression
+* clustering
+* association rules
+* attribute selection
+* experiments
+* workflow
+* visualization
+
+---
+This a repository source for building Weka nupkg for the Chocolatey Package Manager for Windows. 
+To build this:
+
+1. Install git and [chocolatey](https://www.chocolatey.org)
+2. Clone this repo with `git clone`
+3. Package source code to nupkg with `choco pack`
+4. Install using: choco install weka --source <current folder where nupkg is>
+
---
+++ weka/tools/chocolateyinstall.ps1
@@ -0,0 +1,39 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://prdownloads.sourceforge.net/weka/weka-3-8-5.zip' 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $env:ProgramFiles
+  url           = $url
+  softwareName  = 'weka*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '8EEC27669CB6D23BCD844041189067281295D1616F45DABF09F816B093F9FB49'
+  checksumType  = 'sha256' 
+
+}
+
+# Extracted path from zip includes folder hence: $env:ProgramFiles\weka-3-8-5
+
+# Test Java exists on path (required), note javaw not java to not display a console window:
+if ((Get-Command javaw -ErrorAction SilentlyContinue).Name -eq 'javaw.exe') {
+
+Install-ChocolateyZipPackage @packageArgs
+
+[string]$javapath = (Get-Command javaw).Path
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Weka 3.8.5.lnk" `
+  -TargetPath "$javapath" `
+  -WorkingDirectory "$env:ProgramFiles\weka-3-8-5" `
+  -Arguments '-jar weka-3-8-5\weka.jar' `
+  -IconLocation "$env:ProgramFiles\weka-3-8-5\weka.ico" `
+  -Description 'Weka Machine Learning in Java'
+
+
+
+}
+else {
+
+Write-Warning 'No Java runtime found on path on this system. Please amend path or install a version of Java JDK/JRE'
+
+}
