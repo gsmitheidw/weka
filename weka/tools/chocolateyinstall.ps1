32fd1e9108cefbb990c0bf9ae68e0c0a1524b0d1
Graham Smith <gsmitheidw@gmail.com>
2023-01-21 12:38:24 +0000

    updated to 3.8.6

--- weka/tools/chocolateyinstall.ps1
+++ weka/tools/chocolateyinstall.ps1
@@ -1,18 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://prdownloads.sourceforge.net/weka/weka-3-8-5.zip' 
+$url        = 'https://prdownloads.sourceforge.net/weka/weka-3-8-6.zip' 
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $env:ProgramFiles
   url           = $url
   softwareName  = 'weka*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '8EEC27669CB6D23BCD844041189067281295D1616F45DABF09F816B093F9FB49'
+  checksum      = '7E57405331F01C07ED84D9E0FFA07548CDDF70877E4518FCD016C117E8C6E867'
   checksumType  = 'sha256' 
 
 }
 
-# Extracted path from zip includes folder hence: $env:ProgramFiles\weka-3-8-5
+# Extracted path from zip includes folder hence: $env:ProgramFiles\weka-3-8-6
 
 # Test Java exists on path (required), note javaw not java to not display a console window:
 if ((Get-Command javaw -ErrorAction SilentlyContinue).Name -eq 'javaw.exe') {
@@ -22,11 +22,11 @@ Install-ChocolateyZipPackage @packageArgs
 [string]$javapath = (Get-Command javaw).Path
 
 	Install-ChocolateyShortcut `
-	  -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Weka 3.8.5.lnk" `
+	  -ShortcutFilePath "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Weka 3.8.6.lnk" `
 	  -TargetPath "$javapath" `
-	  -WorkingDirectory "$env:ProgramFiles\weka-3-8-5" `
+	  -WorkingDirectory "$env:ProgramFiles\weka-3-8-6" `
 	  -Arguments '-jar weka.jar' `
-	  -IconLocation "$env:ProgramFiles\weka-3-8-5\weka.ico" `
+	  -IconLocation "$env:ProgramFiles\weka-3-8-6\weka.ico" `
 	  -Description 'Weka Machine Learning in Java'
 
 }
--- weka/tools/chocolateyuninstall.ps1
+++ weka/tools/chocolateyuninstall.ps1
@@ -2,8 +2,8 @@
 
 Uninstall-ChocolateyZipPackage `
   -PackageName weka `
-  -ZipFileName weka-3-8-5.zip `
+  -ZipFileName weka-3-8-6.zip `
 
 # Clean up Start Menu Shortcut:
-Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Weka 3.8.5.lnk" -Confirm:$false
+Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Weka 3.8.6.lnk" -Confirm:$false
 
